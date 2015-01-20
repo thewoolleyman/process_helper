6bdfa27e905b30a0d46b1afa4e2e6e0054c0be01
Glenn Oppegard <goppegard@pivotal.io>
2015-01-19 21:57:57 -0700

    [#86484952] :expected_exit_status option handling

--- lib/process_helper.rb
+++ lib/process_helper.rb
@@ -2,6 +2,25 @@ require "process_helper/version"
 require "open3"
 
 module ProcessHelper
+  def handle_exit_status(cmd, options, output, wait_thr)
+    expected_exit_status = options[:expected_exit_status] || 0
+    exit_status = wait_thr.value
+    return if exit_status.exitstatus == expected_exit_status
+
+    if expected_exit_status == 0
+      result_msg = 'failed'
+      exit_status_msg = ''
+    else
+      result_msg = 'succeeded but was expected to fail'
+      exit_status_msg = " (expected #{expected_exit_status})"
+    end
+
+    exception_message = "Command #{result_msg}, #{exit_status}#{exit_status_msg}. " \
+    "Command: `#{cmd}`."
+
+    fail exception_message
+  end
+
   def process(cmd, options = {})
     cmd = cmd.to_s
     fail 'command must not be empty' if cmd.empty?
@@ -10,8 +29,10 @@ module ProcessHelper
       output = ''
       while line = stdout_and_stderr.gets
         output += line
-        puts output unless options[:puts_output] == false
       end
+      puts output unless options[:puts_output] == false
+
+      handle_exit_status(cmd, options, output, wait_thr)
       output
     end
   end
--- spec/process_helper_spec.rb
+++ spec/process_helper_spec.rb
@@ -14,17 +14,17 @@ RSpec.describe do
   end
 
   it "captures stdout only" do
-    output = @clazz.process('echo stdout > /dev/stdout && echo stderr > /dev/null')
+    output = @clazz.process('echo stdout > /dev/stdout && echo stderr > /dev/null', puts_output: false)
     expect(output).to eq("stdout\n")
   end
 
   it "captures stderr only" do
-    output = @clazz.process('echo stdout > /dev/null && echo stderr > /dev/stderr')
+    output = @clazz.process('echo stdout > /dev/null && echo stderr > /dev/stderr', puts_output: false)
     expect(output).to eq("stderr\n")
   end
 
   it "captures stdout and stderr" do
-    output = @clazz.process('echo stdout > /dev/stdout && echo stderr > /dev/stderr')
+    output = @clazz.process('echo stdout > /dev/stdout && echo stderr > /dev/stderr', puts_output: false)
     expect(output).to eq("stdout\nstderr\n")
   end
 
@@ -34,6 +34,40 @@ RSpec.describe do
 
   it "fails if command is empty" do
     expect { @clazz.process('') }.to raise_error('command must not be empty')
+  end
+
+
+  describe "exit status handling" do
+    describe ":expected_exit_status == 0" do
+      describe "when exit_status == 0" do
+        it "succeeds" do
+          expect { @clazz.process('echo', puts_output: false) }.to_not raise_error
+        end
+      end
+
+      describe "when exit_status != 0" do
+        it "fails with message" do
+          expect do
+            @clazz.process('ls /does_not_exist', puts_output: false)
+          end.to raise_error(/Command failed, pid \d+ exit 1\. Command: #{Regexp.escape('`ls /does_not_exist`')}\./)
+        end
+      end
+    end
+
+    describe ":expected_exit_status != 0" do
+      describe "when exit_status == 0" do
+        it "fails with message" do
+          expect do
+            @clazz.process('echo', expected_exit_status: 1, puts_output: false)
+          end.to raise_error(/Command succeeded but was expected to fail, pid \d+ exit 0 \(expected 1\). Command: #{Regexp.escape('`echo`')}\./)
+        end
+      end
+      describe "when exit_status != 0" do
+        it "succeeds" do
+          expect { @clazz.process('ls /does_not_exist', expected_exit_status: 1, puts_output: false) }.to_not raise_error
+        end
+      end
+    end
   end
 
   describe "options" do
