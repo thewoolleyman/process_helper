a13f24c03c9094103ce1f969edf81a7a298be805
Chad <thewoolleyman@gmail.com>
2015-01-19 19:58:44 -0700

    convert to rspec

---
+++ spec/process_helper_spec.rb
@@ -0,0 +1,30 @@
+require_relative 'spec_helper'
+
+class Clazz
+  include ProcessHelper
+end
+
+RSpec.describe do
+  before do
+    @clazz = Clazz.new
+  end
+
+  it "has a version number" do
+    expect(::ProcessHelper::VERSION).to match(/^\d\.\d\.\d\.*\w*$/)
+  end
+
+  it "captures stdout only" do
+    output = @clazz.process('echo stdout > /dev/stdout && echo stderr > /dev/null')
+    expect(output).to eq("stdout\n")
+  end
+
+  it "captures stderr only" do
+    output = @clazz.process('echo stdout > /dev/null && echo stderr > /dev/stderr')
+    expect(output).to eq("stderr\n")
+  end
+
+  it "captures stdout and stderr" do
+    output = @clazz.process('echo stdout > /dev/stdout && echo stderr > /dev/stderr')
+    expect(output).to eq("stdout\nstderr\n")
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require_relative '../lib/process_helper'
+
+# RSpec config
+# RSpec.configure do |c|
+# end
+
+# RSpec helper methods
+# module SpecHelper
+# end
+# include SpecHelper
+
