a7702c45071cd45b1bf3b4e0a46dfb81b073da63
Chad Woolley <thewoolleyman@gmail.com>
2015-04-05 16:45:48 -0700

    remove rubocopy line length ignore override

--- spec/exit_status_handling_spec.rb
+++ spec/exit_status_handling_spec.rb
@@ -19,7 +19,6 @@ RSpec.describe 'exit status handling' do
 
     describe 'when exit_status != 0' do
       it 'fails with message' do
-        # rubocop:disable Metrics/LineLength
         cmd_regex = Regexp.escape('`ls /does_not_exist`')
         expect do
           clazz.process('ls /does_not_exist', puts_output: :exception)
@@ -35,13 +34,13 @@ RSpec.describe 'exit status handling' do
   describe ':expected_exit_status != 0' do
     describe 'when exit_status == 0' do
       it 'fails with message' do
-        # rubocop:disable Metrics/LineLength
+        expected_regex = 'Command succeeded but was expected to fail, ' \
+          'pid \d+ exit 0 \(expected 1\). Command: ' \
+          "#{Regexp.escape('`echo`')}" \
+          '\.'
         expect do
           clazz.process('echo', expected_exit_status: 1, puts_output: :exception)
-        end.to raise_error(
-            ProcessHelper::UnexpectedExitStatusError,
-            /Command succeeded but was expected to fail, pid \d+ exit 0 \(expected 1\). Command: #{Regexp.escape('`echo`')}\./
-          )
+        end.to raise_error(ProcessHelper::UnexpectedExitStatusError, /#{expected_regex}/)
             .and(output("\n").to_stdout)
       end
     end
